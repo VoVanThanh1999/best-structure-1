f3dabe2d4b5ab2d9adce6e8ba721d8c9cf73a286
Võ Văn Thành <46281439+VoVanThanh1999@users.noreply.github.com>
2022-03-20 21:21:35 +0700

    Initialize greenkeeper/@apollo/gateway-0.11.0

---
+++ src/app.module.ts
@@ -0,0 +1,75 @@
+import { Module, CacheModule } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { GraphQLModule } from '@nestjs/graphql'
+
+import { AppController } from './app.controller'
+import { CacheService, TypeormService, GraphqlService } from './config'
+import { DateScalar } from './config/graphql/scalars/date.scalar'
+import { UploadScalar } from './config/graphql/scalars/upload.scalar'
+
+import {
+	UserResolver,
+	ResultResolver,
+	UserResultResolver,
+	AuthResolver,
+	EmailResolver,
+	FileResolver,
+	PermissionResolver,
+	RoleResolver,
+	NotificationResolver,
+	RoomResolver,
+	MessageResolver,
+	FormResolver,
+	NodeResolver,
+	TranslateResolver,
+	CompanyResolver,
+	CityResolver,
+	StoreResolver,
+	DepartmentResolver,
+	PositionResolver
+} from './resolvers'
+
+@Module({
+	imports: [
+		GraphQLModule.forRootAsync({
+			useClass: GraphqlService
+		}),
+		TypeOrmModule.forRootAsync({
+			useClass: TypeormService
+		}),
+		CacheModule.registerAsync({
+			useClass: CacheService
+		})
+	],
+	controllers: [AppController],
+	providers: [
+		DateScalar,
+		UploadScalar,
+		UserResolver,
+		ResultResolver,
+		UserResultResolver,
+		AuthResolver,
+		EmailResolver,
+		FileResolver,
+		PermissionResolver,
+		RoleResolver,
+		NotificationResolver,
+		RoomResolver,
+		MessageResolver,
+		FormResolver,
+		NodeResolver,
+		TranslateResolver,
+		CompanyResolver,
+		CityResolver,
+		StoreResolver,
+		DepartmentResolver,
+		PositionResolver
+	]
+})
+export class AppModule {}
+
+// const a = [1,2]
+// const b = [3,4]
+
+// const c = [1,2, ...b]
+// const d = [1,2,3,4]
---
+++ src/models/permission.entity.ts
@@ -0,0 +1,34 @@
+import { Entity, ObjectIdColumn, Column } from 'typeorm'
+import * as uuid from 'uuid'
+import { Expose, plainToClass } from 'class-transformer'
+
+@Entity({
+	name: 'permissions',
+	orderBy: {
+		createdAt: 'ASC'
+	}
+})
+export class Permission {
+	@Expose()
+	@ObjectIdColumn()
+	_id: string
+
+	@Expose()
+	@Column()
+	code: string
+
+	@Expose()
+	@Column()
+	description: string
+
+	@Expose()
+	@Column()
+	isActive: boolean
+
+	@Expose()
+	@Column()
+	createdAt: number
+	@Expose()
+	@Column()
+	updatedAt: number
+}
