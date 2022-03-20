c570fcc40ef48ae0d0add36519b6cbd43bfa8b60
Võ Văn Thành <46281439+VoVanThanh1999@users.noreply.github.com>
2022-03-20 21:38:43 +0700

    Initialize dependabot/npm_and_yarn/progress-bar-webpack-plugin-2.1.0

---
+++ src/resolvers/result.resolver.ts
@@ -0,0 +1,15 @@
+import { Resolver, ResolveField } from '@nestjs/graphql'
+
+@Resolver('Result')
+export class ResultResolver {
+	@ResolveField()
+	__resolveType(obj, context, info) {
+		if (obj.email) {
+			return 'User'
+		}
+		if (obj.filename) {
+			return 'File'
+		}
+		return null
+	}
+}
